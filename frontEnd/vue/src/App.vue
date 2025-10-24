5cd3874651ba7b73e835bb5c8484b8e76ee8901e
Crist Yang <cr1st4ever@outlook.com>
2025-10-24 20:01:24 +0800

    create vue

--- frontEnd/vue/src/App.vue
+++ frontEnd/vue/src/App.vue
@@ -3,11 +3,7 @@
 </script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blannoopenoopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <h1>Hello Foreveryang.cn</h1>
 </template>
 
 <style>
