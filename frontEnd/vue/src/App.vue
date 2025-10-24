7a8efee669e839070d8d00ed0ae74c4110f9a393
Crist Yang <cr1st4ever@outlook.com>
2025-10-24 19:59:23 +0800

    create vue

---
+++ frontEnd/vue/src/App.vue
@@ -0,0 +1,15 @@
+<script lang="ts">
+
+</script>
+
+<template>
+  <h1>You did it!</h1>
+  <p>
+    Visit <a href="https://vuejs.org/" target="_blannoopenoopener">vuejs.org</a> to read the
+    documentation
+  </p>
+</template>
+
+<style>
+
+</style>
